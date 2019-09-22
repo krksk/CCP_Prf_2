982944d90b8d31ddfb7ea05fafc91e3588f43b78
krksk <cooling343@gmail.com>
2019-09-22 17:15:48 +0900

    99

--- source/example.js
+++ source/example.js
@@ -1,14 +1,20 @@
 window.onscroll = function(){
   var t = document.documentElement.scrollTop;
-    if(t>200){
+    if(t>600){
+    title.style.animationPlayState = "running";
     a1.style.animationPlayState = "running";
     a2.style.animationPlayState = "running";
     a3.style.animationPlayState = "running";
     a4.style.animationPlayState = "running";
     field.style.animationPlayState = "running";
-    }
-    else if(t>6000){
+    }if(t>5000){
+    dP_montage_headline.style.animationPlayState = "running";
+    dP_headline_Picture.style.animationPlayState = "running";
+    }if(t>7800){
     suspicion_substance1.style.animationPlayState = "running";
     suspicion_substance2.style.animationPlayState = "running";
-    suspicion_substance3.style.animationPlayState = "running";
-  }}
+    }if(t>10000){
+    ending_headline.style.animationPlayState = "running";
+    }
+    monitor.innerHTML = t + "px";
+    }
