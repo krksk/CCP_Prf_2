7caf8dfc60f41b7363d6954017b0093c41c2826c
krksk <cooling343@gmail.com>
2019-09-23 18:22:51 +0900

    5

--- source/example.js
+++ source/example.js
@@ -7,6 +7,20 @@ window.onscroll = function(){
     a3.style.animationPlayState = "running";
     a4.style.animationPlayState = "running";
     field.style.animationPlayState = "running";
+    }if(t>800){
+    introduce.style.animationPlayState = "running";
+    develop.style.animationPlayState = "paused";
+    analysis.style.animationPlayState = "paused";
+    doubt.style.animationPlayState = "paused";
+    now.style.animationPlayState = "paused";
+    }if(t>1900){
+    introduce.style.animationPlayState = "paused";
+    }if(t>4800){
+    analysis.style.animationPlayState = "running";
+    }if(t>8500){
+    doubt.style.animationPlayState = "running";
+    }if(t>10750){
+    now.style.animationPlayState = "running";
     }if(t>5000){
     dP_montage_headline.style.animationPlayState = "running";
     dP_headline_Picture.style.animationPlayState = "running";
@@ -15,6 +29,7 @@ window.onscroll = function(){
     suspicion_substance2.style.animationPlayState = "running";
     }if(t>10000){
     ending_headline.style.animationPlayState = "running";
+    endingPicture.style.animationPlayState = "running";
     }
     monitor.innerHTML = t + "px";
     }
